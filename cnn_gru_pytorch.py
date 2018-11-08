037b76b8b5a43690ab2bb38833ff525a60c290b8
cyh <cyh.wne@gmail.com>
2018-11-08 11:13:32 +0800

    add class TCN

--- cnn_gru_pytorch.py
+++ cnn_gru_pytorch.py
@@ -132,6 +132,69 @@ class ResNet(nn.Module):
 
         return x,feature
 
+class Chomp1d(nn.Module):
+    def __init__(self, chomp_size):
+        super(Chomp1d, self).__init__()
+        self.chomp_size = chomp_size
+
+    def forward(self, x):
+        return x[:, :, :-self.chomp_size].contiguous()
+
+class TemporalBlock(nn.Module):
+    def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
+        super(TemporalBlock, self).__init__()
+        self.conv1 = weight_norm(nn.Conv1d(n_inputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation))
+        self.chomp1 = Chomp1d(padding)
+        self.relu1 = nn.ReLU()
+        self.dropout1 = nn.Dropout(dropout)
+
+        self.conv2 = weight_norm(nn.Conv1d(n_outputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation))
+        self.chomp2 = Chomp1d(padding)
+        self.relu2 = nn.ReLU()
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1, self.dropout1,
+                                 self.conv2, self.chomp2, self.relu2, self.dropout2)
+        self.downsample = nn.Conv1d(n_inputs, n_outputs, 1) if n_inputs != n_outputs else None
+        self.relu = nn.ReLU()
+        self.init_weights()
+
+    def init_weights(self):
+        self.conv1.weight.data.normal_(0, 0.01)
+        self.conv2.weight.data.normal_(0, 0.01)
+        if self.downsample is not None:
+            self.downsample.weight.data.normal_(0, 0.01)
+
+    def forward(self, x):
+        out = self.net(x)
+        res = x if self.downsample is None else self.downsample(x)
+        return self.relu(out + res)
+
+class TCN(nn.Module):
+    def __init__(self, num_input_channel, num_channels, num_block=None, kernel_size=3, dropout=0.1):
+        super(TCN, self).__init__()
+        layers = []
+        num_levels = len(num_channels)
+        num_block = [1]*len(num_channels) if num_block is None else num_block
+        for i in range(num_levels):
+            dilation_size = 2 ** i
+            in_channels = num_input_channel if i == 0 else num_channels[i-1]
+            out_channels = num_channels[i]
+            for j in range(num_block[i]):
+                in_channels = num_channels[i] if j > 0 else in_channels
+                layers += [TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
+                                        padding=(kernel_size-1) * dilation_size, dropout=dropout)]
+
+        self.network = nn.Sequential(*layers)
+        self.linear = nn.Linear(num_channels[-1],1)
+    def forward(self, x):
+        x =  self.network(x)
+        feature = x[:,:,-1].contiguous().view(x.size(0),-1)
+        out = self.linear(feature)
+        return out,feature
+
 class CNN(nn.Module):
     def __init__(self, feature_size):
         super(CNN, self).__init__()
