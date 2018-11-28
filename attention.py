d264aea443b5edc5c22ebbe13a8e1b59937b41b9
cyh <cyh.wne@gmail.com>
2018-11-28 18:52:43 +0800

    deal with some warning and add function to plot result

--- attention.py
+++ attention.py
@@ -2,11 +2,12 @@ import random
 import numpy as np 
 from collections import OrderedDict
 import math
+import matplotlib.pyplot as plt 
 import torch
 from torch import nn, optim
 from torch.autograd import Variable
 import torch.nn.functional as F
-from torch.nn.utils import clip_grad_norm
+from torch.nn.utils import clip_grad_norm_
 from dataset import DataSet
 
 '''
@@ -21,14 +22,11 @@ class Encoder(nn.Module):
         super(Encoder, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
-        # self.embed = nn.Embedding(input_size, embed_size)
         self.gru = nn.GRU(input_size, hidden_size, n_layers,
                           dropout=dropout, bidirectional=True)
 
     def forward(self, x, hidden=None):
-        # embedded = self.embed(src)
         outputs, hidden = self.gru(x, hidden)
-        # sum bidirectional outputs
         outputs = (outputs[:, :, :self.hidden_size] +
                    outputs[:, :, self.hidden_size:])
         return outputs, hidden
@@ -52,7 +50,7 @@ class Attention(nn.Module):
 
     def score(self, hidden, encoder_outputs):
         # [B*T*2H]->[B*T*H]
-        energy = F.softmax(self.attn(torch.cat([hidden, encoder_outputs], 2)))
+        energy = F.softmax(self.attn(torch.cat([hidden, encoder_outputs], 2)),dim=2)
         energy = energy.transpose(1, 2)  # [B*H*T]
         v = self.v.repeat(encoder_outputs.size(0), 1).unsqueeze(1)  # [B*1*H]
         energy = torch.bmm(v, energy)  # [B*1*T]
@@ -63,13 +61,10 @@ class Decoder(nn.Module):
     def __init__(self, hidden_size, output_size,
                  n_layers=1, dropout=0.2):
         super(Decoder, self).__init__()
-        # self.embed_size = embed_size
         self.hidden_size = hidden_size
         self.output_size = output_size
         self.n_layers = n_layers
 
-        # self.embed = nn.Embedding(output_size, hidden_size)
-        # self.dropout = nn.Dropout(dropout, inplace=True)
         self.attention = Attention(hidden_size)
         self.gru = nn.GRU(hidden_size + output_size, hidden_size,
                           n_layers, dropout=dropout)
@@ -79,7 +74,6 @@ class Decoder(nn.Module):
         # Get the embedding of the current input word (last output word)
         # embedded = self.embed(input).unsqueeze(0)  # (1,B,N)
         embedded = input.unsqueeze(0)
-        # embedded = self.dropout(embedded)
         # Calculate attention weights and apply to encoder outputs
         attn_weights = self.attention(last_hidden[-1], encoder_outputs)
         context = attn_weights.bmm(encoder_outputs.transpose(0, 1))  # (B,1,N)
@@ -114,14 +108,13 @@ class Seq2Seq(nn.Module):
                     output, hidden, encoder_output)
             outputs[t] = output
             is_teacher = random.random() < teacher_forcing_ratio
-            # top1 = output.data.max(1)[1]
             output = Variable(trg.data[t,] if is_teacher else output).cuda()
         return outputs
 
 
 class RUL():
     def __init__(self):
-        self.hidden_size = 32
+        self.hidden_size = 16
         self.epochs = 50
         self.lr = 1e-3
         self.dataset = DataSet.load_dataset(name='phm_data')
@@ -130,57 +123,7 @@ class RUL():
                                 'Bearing2_3','Bearing2_4','Bearing2_5','Bearing2_6','Bearing2_7',
                                 'Bearing3_3']
 
-    def _evaluate(self, model, val_iter):
-        model.eval()
-        total_loss = 0
-        for [data, label] in val_iter:
-            # src, len_src = batch.src
-            # trg, len_trg = batch.trg
-            data, label = torch.from_numpy(data), torch.from_numpy(label)
-            data, label = data.type(torch.FloatTensor), label.type(torch.FloatTensor)
-            data = Variable(data, volatile=True).cuda()
-            label = Variable(label, volatile=True).cuda()
-            output = model(data, label, teacher_forcing_ratio=0.0)
-            # loss = F.nll_loss(output[1:].view(-1, vocab_size),
-            #                     trg[1:].contiguous().view(-1),
-            #                     ignore_index=pad)
-            loss = F.mse_loss(output,label)                    
-            total_loss += loss.data[0]
-        return total_loss / len(val_iter)
-
-
-    def _fit(self, e, model, optimizer, train_iter, grad_clip=10.0):
-        model.train()
-        total_loss = 0
-        # idx = [i for i in range(len(train_iter))]
-        # random.shuffle(idx)
-        # train_iter = train_iter[idx]
-        random.shuffle(train_iter)
-        for [data, label] in train_iter:
-            # src, len_src = batch.src
-            # trg, len_trg = batch.trg
-            # src, trg = src.cuda(), trg.cuda()
-            data, label = torch.from_numpy(data), torch.from_numpy(label)
-            data, label = data.type(torch.FloatTensor), label.type(torch.FloatTensor)
-            data, label = Variable(data).cuda(), Variable(label).cuda()
-            optimizer.zero_grad()
-            output = model(data, label)
-            # loss = F.nll_loss(output[1:].view(-1, vocab_size),
-            #                     trg[1:].contiguous().view(-1),
-            #                     ignore_index=pad)
-            loss = F.mse_loss(output,label)
-            loss.backward()
-            clip_grad_norm(model.parameters(), grad_clip)
-            optimizer.step()
-            total_loss += loss.data[0]
-        print('[Epoch:%d][loss:%5.2f]' % (e,total_loss))
-            # if b % 100 == 0 and b != 0:
-            #     total_loss = total_loss / 100
-            #     print("[%d][loss:%5.2f][pp:%5.2f]" %
-            #         (b, total_loss, math.exp(total_loss)))
-            #     total_loss = 0
-
-
+    
     def train(self):
         train_data,train_label = self._preprocess('train')
         train_iter = [[train_data[i],train_label[i]] for i in range(len(train_data))]
@@ -188,27 +131,91 @@ class RUL():
         val_iter = [[test_data[i],test_label[i]] for i in range(len(test_data))]
 
         encoder = Encoder(self.feature_size,self.hidden_size,n_layers=2,dropout=0.5)
-        decoder = Decoder(self.hidden_size,1,n_layers=1,dropout=0.5)
+        decoder = Decoder(self.hidden_size,1,n_layers=1)
         seq2seq = Seq2Seq(encoder,decoder).cuda()
         optimizer = optim.Adam(seq2seq.parameters(), lr=self.lr)
 
-        # best_val_loss = None
         for e in range(self.epochs):
-            self._fit(e, seq2seq, optimizer, train_iter)
+            train_loss = self._fit(e, seq2seq, optimizer, train_iter)
             val_loss = self._evaluate(seq2seq, val_iter)
-            print("[Epoch:%d][val_loss:%.3f] "
-                % (e, val_loss))
-
-            # Save the model if the validation loss is the best we've seen so far.
-        #     if not best_val_loss or val_loss < best_val_loss:
-        #         print("[!] saving model...")
-        #         if not os.path.isdir(".save"):
-        #             os.makedirs(".save")
-        #         torch.save(seq2seq.state_dict(), './.save/seq2seq_%d.pt' % (e))
-        #         best_val_loss = val_loss
-        # test_loss = self._evaluate(seq2seq, test_iter, en_size)
-        # print("[TEST] loss:%5.2f" % test_loss)
+            print("[Epoch:%d][train_loss:%.3f][val_loss:%.3f] "
+                % (e, train_loss, val_loss))
+            if e % 10 == 0:
+                self._plot_result(seq2seq, train_iter, val_iter)
         
+    def _evaluate(self, model, val_iter):
+        model.eval()
+        total_loss = 0
+        for [data, label] in val_iter:
+            with torch.no_grad():
+                data, label = torch.from_numpy(data), torch.from_numpy(label)
+                data, label = data.type(torch.FloatTensor), label.type(torch.FloatTensor)
+                data = Variable(data).cuda()
+                label = Variable(label).cuda()
+                output = model(data, label, teacher_forcing_ratio=0.0)
+            loss = F.mse_loss(output,label)                    
+            total_loss += loss.data
+        return total_loss / len(val_iter)
+
+
+    def _fit(self, e, model, optimizer, train_iter, grad_clip=10.0):
+        model.train()
+        total_loss = 0
+        random.shuffle(train_iter)
+        for [data, label] in train_iter:
+            data, label = torch.from_numpy(data), torch.from_numpy(label)
+            data, label = data.type(torch.FloatTensor), label.type(torch.FloatTensor)
+            data, label = Variable(data).cuda(), Variable(label).cuda()
+            optimizer.zero_grad()
+            output = model(data, label)
+            loss = F.mse_loss(output,label)
+            loss.backward()
+            clip_grad_norm_(model.parameters(), grad_clip)
+            optimizer.step()
+            total_loss += loss.data
+        return total_loss / len(train_iter)
+
+
+    def _plot_result(self, model, train_iter, val_iter):
+        model.eval()
+
+        labels = []
+        outputs = []
+        with torch.no_grad():
+            for [data, label] in train_iter:
+                data, label = torch.from_numpy(data), torch.from_numpy(label)
+                data, label = data.type(torch.FloatTensor), label.type(torch.FloatTensor)
+                data = Variable(data).cuda()
+                label = Variable(label).cuda()
+                output = model(data, label, teacher_forcing_ratio=0.0)
+                labels.append(label.data.cpu().numpy())
+                outputs.append(output.data.cpu().numpy())
+        labels = np.concatenate(tuple(x for x in labels), axis=0)
+        outputs = np.concatenate(tuple(x for x in outputs), axis=0)
+        labels, outputs = labels.reshape(-1,), outputs.reshape(-1,)
+        plt.subplot(2,1,1)
+        plt.plot(labels)
+        plt.plot(outputs)
+
+        labels = []
+        outputs = []
+        with torch.no_grad():
+            for [data, label] in val_iter:
+                data, label = torch.from_numpy(data), torch.from_numpy(label)
+                data, label = data.type(torch.FloatTensor), label.type(torch.FloatTensor)
+                data = Variable(data).cuda()
+                label = Variable(label).cuda()
+                output = model(data, label, teacher_forcing_ratio=0.0)
+                labels.append(label.data.cpu().numpy())
+                outputs.append(output.data.cpu().numpy())
+        labels = np.concatenate(tuple(x for x in labels), axis=0)
+        outputs = np.concatenate(tuple(x for x in outputs), axis=0)
+        labels, outputs = labels.reshape(-1,), outputs.reshape(-1,)
+        plt.subplot(2,1,2)
+        plt.plot(labels)
+        plt.plot(outputs)
+        plt.show()
+
     
     def _preprocess(self, select):
         if select == 'train':
@@ -223,12 +230,10 @@ class RUL():
         for i,x in enumerate(temp_label):
             temp_label[i] = np.arange(temp_data[i].shape[0])[::-1] + x
             temp_label[i] = temp_label[i][:,np.newaxis,np.newaxis]
+            temp_label[i] = temp_label[i] / np.max(temp_label[i])
         for i,x in enumerate(temp_data):
             temp_data[i] = x.transpose(0,2,1)
         time_feature = [self._get_time_fea(x) for x in temp_data]
-        # for i in range(len(time_feature)):
-        #     time_feature[i] = time_feature[i][-300:,:,:]
-        #     temp_label[i] = temp_label[i][-300:,]
         return time_feature, temp_label
 
     def _get_time_fea(self, data):
@@ -255,11 +260,20 @@ class RUL():
         fea = np.concatenate(tuple(x for x in fea_dict.values()),axis=2)
         fea = fea.reshape(-1,fea.shape[1]*fea.shape[2])
         self.feature_size = fea.shape[1]
+        fea = self._normalize(fea,dim=1)
         fea = fea[:,np.newaxis,:]
         return fea
     
     def _get_fre_fea(self, data):
         pass
+
+    def _normalize(self, data, dim=None):
+        if dim == None:
+            r_data = (data - np.min(data)) / (np.max(data) - np.min(data))
+        else:
+            r_data = (data - np.min(data,axis=dim,keepdims=True).repeat(data.shape[dim],axis=dim)) \
+                / (np.max(data,axis=dim,keepdims=True) - np.min(data,axis=dim,keepdims=True)).repeat(data.shape[dim],axis=dim)
+        return r_data
 
 
 if __name__ == '__main__':
