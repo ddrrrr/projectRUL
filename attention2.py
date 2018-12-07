6b707d4c2c22524ef8ce62c9b7804047cc48adc3
cyh <cyh.wne@gmail.com>
2018-12-07 20:43:47 +0800

    attention2.py ; add analyse function in attention.py

--- attention2.py
+++ attention2.py
@@ -19,19 +19,20 @@ from dataset import DataSet
 '''
 
 class Encoder(nn.Module):
-    def __init__(self, input_size, hidden_size,
+    def __init__(self, input_size, conv_size, hidden_size,
                  n_layers=1, dropout=0.5):
         super(Encoder, self).__init__()
         self.input_size = input_size
+        self.conv_size = conv_size
         self.hidden_size = hidden_size
-        self.cnn_kernel_size = 64
-        self.cnn_strides = 5   # when chang 10
+        self.cnn_kernel_size = 32
+        self.cnn_strides = 5
         self.cnn = nn.Sequential(
-            nn.Conv1d(self.input_size, 64, self.cnn_kernel_size, self.cnn_strides),
+            nn.Conv1d(self.input_size, self.conv_size, self.cnn_kernel_size, self.cnn_strides),
             # nn.ReLU()
             nn.PReLU()
             )
-        self.gru = nn.GRU(64, hidden_size, n_layers,
+        self.gru = nn.GRU(self.conv_size, hidden_size, n_layers,
                           dropout=dropout, bidirectional=True)
 
     def forward(self, x, hidden=None):
@@ -39,11 +40,11 @@ class Encoder(nn.Module):
         padding = self.cnn_kernel_size - x.size(2) % self.cnn_strides
         x = F.pad(x, (0,padding))
         x = self.cnn(x)
-        x = x.permute(2,0,1).contiguous()  # [T*B*N]
-        outputs, hidden = self.gru(x, hidden)
+        conv_output = x.permute(2,0,1).contiguous()  # [T*B*N]
+        outputs, hidden = self.gru(conv_output, hidden)
         outputs = (outputs[:, :, :self.hidden_size] +
                    outputs[:, :, self.hidden_size:])
-        return outputs, hidden
+        return conv_output, outputs, hidden
 
 
 class Attention(nn.Module):
@@ -55,12 +56,12 @@ class Attention(nn.Module):
         stdv = 1. / math.sqrt(self.v.size(0))
         self.v.data.uniform_(-stdv, stdv)
 
-    def forward(self, hidden, encoder_outputs):
+    def forward(self, encoder_outputs, hidden):
         timestep = encoder_outputs.size(0)
         h = hidden.repeat(timestep, 1,  1).transpose(0, 1)
         encoder_outputs = encoder_outputs.transpose(0, 1)  # [B*T*H]
         attn_energies = self.score(h, encoder_outputs)
-        return F.relu(attn_energies).unsqueeze(1)
+        return F.relu(attn_energies).transpose(0,1).unsqueeze(2) # T*B*1
 
     def score(self, hidden, encoder_outputs):
         # [B*T*2H]->[B*T*H]
@@ -72,80 +73,51 @@ class Attention(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, hidden_size, output_size,
+    def __init__(self, conv_size, hidden_size,
                  n_layers=1, dropout=0.2):
         super(Decoder, self).__init__()
+        self.conv_size = conv_size
         self.hidden_size = hidden_size
-        self.output_size = output_size
         self.n_layers = n_layers
 
         self.attention = Attention(hidden_size)
-        self.gru = nn.GRU(hidden_size + output_size, hidden_size,
+        self.gru = nn.GRU(conv_size, hidden_size,
                           n_layers, dropout=dropout)
-        self.out = nn.Linear(hidden_size * 2, output_size)
+        self.out = nn.Sequential(
+            nn.Linear(self.hidden_size, 64),
+            nn.PReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(64, 1),
+            nn.Sigmoid()
+        )
 
-    def forward(self, input, last_hidden, encoder_outputs):
-        # Get the embedding of the current input word (last output word)
-        # embedded = self.embed(input).unsqueeze(0)  # (1,B,N)
-        embedded = input.unsqueeze(0)
-        # Calculate attention weights and apply to encoder outputs
-        attn_weights = self.attention(last_hidden[-1], encoder_outputs)
-        context = attn_weights.bmm(encoder_outputs.transpose(0, 1))  # (B,1,N)
-        context = context.transpose(0, 1)  # (1,B,N)
-        # Combine embedded input word and attended context, run through RNN
-        rnn_input = torch.cat([embedded, context], 2)
-        output, hidden = self.gru(rnn_input, last_hidden)
-        output = output.squeeze(0)  # (1,B,N) -> (B,N)
-        context = context.squeeze(0)
-        output = self.out(torch.cat([output, context], 1))
-        # output = F.log_softmax(output, dim=1)
-        return output, hidden, attn_weights
+    def forward(self, conv_output, encoder_output, e_hidden):
+        atten_weights = self.attention(encoder_output, e_hidden[-1])  # T*B*1
+        context = atten_weights.repeat(1,1,conv_output.size(2)).mul(conv_output)    # T*B*N
+        d_output, d_hidden = self.gru(conv_output, None)
+        d_hidden = d_hidden[-1,:,:]    # B*H
+        output = self.out(d_hidden)
+        return atten_weights, output
 
 
 class Seq2Seq(nn.Module):
-    def __init__(self, encoder, decoder, teacher_forcing_ratio=0.5):
+    def __init__(self, encoder, decoder):
         super(Seq2Seq, self).__init__()
         self.encoder = encoder
         self.decoder = decoder
-        self.teacher_forcing_ratio = teacher_forcing_ratio
 
-    def forward(self, src, trg, teacher_forcing_ratio=None, is_analyse=False):
-        batch_size = src.size(1)
-        max_len = trg.size(0)
-        vocab_size = self.decoder.output_size
-        outputs = Variable(torch.zeros(max_len, batch_size, vocab_size)).cuda()
-
-        encoder_output, hidden = self.encoder(src)
-        hidden = hidden[:self.decoder.n_layers]
-        output = Variable(trg.data[0,])  # sos
-
-        if is_analyse:
-            analyse_data = OrderedDict()
-            analyse_data['fea_after_encoder'] = encoder_output.data.cpu().numpy()
-            analyse_data['atten'] = []
-        for t in range(1, max_len):
-            output, hidden, attn_weights = self.decoder(
-                    output, hidden, encoder_output)
-            outputs[t] = output
-            if teacher_forcing_ratio == None:
-                teacher_forcing_ratio = self.teacher_forcing_ratio
-            is_teacher = random.random() < teacher_forcing_ratio
-            output = Variable(trg.data[t,] if is_teacher else output).cuda()
-            if is_analyse:
-                analyse_data['atten'].append(attn_weights.data.cpu().numpy())
-        if is_analyse:
-            analyse_data['atten'] = np.concatenate(analyse_data['atten'],axis=0)
-            return outputs, analyse_data
-        else:
-            return outputs
+    def forward(self, x, is_analyse=False):
+        conv_output, encoder_output, hidden = self.encoder(x)
+        atten, out = self.decoder(conv_output, encoder_output, hidden)
+        return out
 
 
 class RUL():
     def __init__(self):
         self.hidden_size = 200
+        self.conv_size = 64
         self.epochs = 400
-        self.lr = 1e-3
-        self.gama = 0.7
+        self.lr = 1e-2
         self.dataset = DataSet.load_dataset(name='phm_data')
         self.train_bearings = ['Bearing1_1','Bearing1_2','Bearing2_1','Bearing2_2','Bearing3_1','Bearing3_2']
         self.test_bearings = ['Bearing1_3','Bearing1_4','Bearing1_5','Bearing1_6','Bearing1_7',
@@ -159,51 +131,35 @@ class RUL():
         test_data,test_label = self._preprocess('test')
         val_iter = [[test_data[i],test_label[i]] for i in range(len(test_data))]
 
-        encoder = Encoder(self.feature_size,self.hidden_size,n_layers=2,dropout=0.5)
-        decoder = Decoder(self.hidden_size,1,n_layers=2,dropout=0.5)
+        encoder = Encoder(self.feature_size,self.conv_size,self.hidden_size,n_layers=2,dropout=0.5)
+        decoder = Decoder(self.conv_size,self.hidden_size,n_layers=2,dropout=0.5)
         seq2seq = Seq2Seq(encoder,decoder).cuda()
         # seq2seq = torch.load('./model/newest_seq2seq')
-        seq2seq.teacher_forcing_ratio = 0.3
         optimizer = optim.Adam(seq2seq.parameters(), lr=self.lr)
 
         log = OrderedDict()
         log['train_loss'] = []
         log['val_loss'] = []
         log['test_loss'] = []
-        log['teacher_ratio'] = []
         count = 0
-        count2 = 0
         for e in range(1, self.epochs+1):
             train_loss = self._fit(e, seq2seq, optimizer, train_iter)
             val_loss = self._evaluate(seq2seq, train_iter)
             test_loss = self._evaluate(seq2seq, val_iter)
-            print("[Epoch:%d][train_loss:%.4e][val_loss:%.4e][test_loss:%.4e][teacher_ratio:%.4f] "
-                % (e, train_loss, val_loss, test_loss, seq2seq.teacher_forcing_ratio))
+
+            print("[Epoch:%d][train_loss:%.4e][val_loss:%.4e][test_loss:%.4e]"
+                    % (e, train_loss, val_loss, test_loss))
             log['train_loss'].append(float(train_loss))
             log['val_loss'].append(float(val_loss))
             log['test_loss'].append(float(test_loss))
-            log['teacher_ratio'].append(seq2seq.teacher_forcing_ratio)
             pd.DataFrame(log).to_csv('./model/log.csv',index=False)
             if float(val_loss) == min(log['val_loss']):
-                torch.save(seq2seq, './model/seq2seq')
-            torch.save(seq2seq, './model/newest_seq2seq')
-
-            count2 += 1
-            if float(train_loss) <= float(val_loss)*0.2:
-                count += 1
+                torch.save(seq2seq, './model/atten2')
             else:
-                count = 0
-            if count >= 3 or count2 >= 100:
-                seq2seq.teacher_forcing_ratio *= self.gama
-                count -= 1
-                count2 = 0
-            
-            if (count2+1) % 40 == 0:
-                optimizer.param_groups[0]['lr'] *= 0.9
-                
-
-            # if e % 20 == 0:
-            #     self._plot_result(seq2seq, train_iter, val_iter)
+                count += 1
+            torch.save(seq2seq, './model/newest_atten2')
+            if count > 10:
+                optimizer.param_groups[0]['lr'] *= 0.8
 
     def test(self):
         train_data,train_label = self._preprocess('train')
@@ -211,7 +167,7 @@ class RUL():
         test_data,test_label = self._preprocess('test')
         val_iter = [[test_data[i],test_label[i]] for i in range(len(test_data))]
 
-        seq2seq = torch.load('./model/seq2seq')
+        seq2seq = torch.load('./model/atten2')
         self._plot_result(seq2seq, train_iter, val_iter)
 
     def analyse(self):
@@ -264,18 +220,22 @@ class RUL():
         sio.savemat('analyse_data.mat',analyse_data)
 
         
+    def _custom_loss(self, output, label, size):
+        return (size/(1-output)-label)**2
+
     def _evaluate(self, model, val_iter):
         model.eval()
-        # [data, label] = random.choice(val_iter)
         total_loss = 0
         for [data, label] in val_iter:
             with torch.no_grad():
+                label = label[0:1,]
                 data, label = torch.from_numpy(data.copy()), torch.from_numpy(label.copy())
                 data, label = data.type(torch.FloatTensor), label.type(torch.FloatTensor)
                 data = Variable(data).cuda()
                 label = Variable(label).cuda()
-                output = model(data, label, teacher_forcing_ratio=0.0)
-            loss = F.mse_loss(output,label)
+                output = model(data)
+            # loss = self._custom_loss(output,label,data.size(0))
+            loss = F.mse_loss(output, label)
             total_loss += loss.data
         return total_loss / len(val_iter)
 
@@ -285,21 +245,18 @@ class RUL():
         total_loss = 0
         random.shuffle(train_iter)
         for [data, label] in train_iter:
-            random_idx = random.randint(0,label.shape[0]-5)
-            # data_len, label_len = data.shape[0]-random_idx*5, label.shape[0]-random_idx
-            # cat_data, cat_label = [], []
-            # for i in range(min(random_idx+1,12)):
-            #     cat_data.append(data[i*5:i*5+data_len,])    # when chang 10
-            #     cat_label.append(label[i:i+label_len,])
-            # data = np.concatenate(cat_data, axis=1)
-            # label = np.concatenate(cat_label, axis=1)
-            data, label = data[random_idx*5:,], label[random_idx:,]  # when chang 10
+            random_idx = random.randint(0,round(data.shape[0]-350))
+            random_len = random.randint(100,data.shape[0] - random_idx - 100)
+            data = np.concatenate(tuple(data[i+random_idx:i+random_idx+random_len,] for i in range(64)),axis=1)
+            label = np.concatenate(tuple(label[i+random_idx:i+random_idx+1] for i in range(64)),axis=0)
+            # data, label = data[random_idx:,], label[random_idx:random_idx+1,]
             data, label = torch.from_numpy(data.copy()), torch.from_numpy(label.copy())
             data, label = data.type(torch.FloatTensor), label.type(torch.FloatTensor)
             data, label = Variable(data).cuda(), Variable(label).cuda()
             optimizer.zero_grad()
-            output = model(data, label)
-            loss = F.mse_loss(output,label)
+            output = model(data)
+            # loss = self._custom_loss(output,label,data.size(0))
+            loss = F.mse_loss(output, label)
             loss.backward()
             clip_grad_norm_(model.parameters(), grad_clip)
             optimizer.step()
@@ -361,10 +318,10 @@ class RUL():
             raise ValueError('wrong selection!')
 
         for i,x in enumerate(temp_label):
-            temp_label[i] = np.arange(temp_data[i].shape[0]) + x
-            temp_label[i] = temp_label[i][:,np.newaxis,np.newaxis]
+            temp_label[i] = np.arange(temp_data[i].shape[0],dtype=np.float64) + x
+            temp_label[i] = temp_label[i][:,np.newaxis]
             temp_label[i] = temp_label[i] / np.max(temp_label[i])
-            temp_label[i] = temp_label[i][::5] # when chang 10
+            # temp_label[i] = temp_label[i][::5] # when chang 10
         for i,x in enumerate(temp_data):
             temp_data[i] = x[::-1,].transpose(0,2,1)
         time_feature = [self._get_time_fea(x) for x in temp_data]
@@ -420,5 +377,5 @@ class RUL():
 if __name__ == '__main__':
     process = RUL()
     process.train()
-    process.test()
+    # process.test()
     # process.analyse()
